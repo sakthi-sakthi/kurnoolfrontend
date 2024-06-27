c2f9dd747293f29ec75671751e0c4763126bb368
sakthi-sakthi <sakthiganapathis97@gmail.com>
2024-06-27 18:35:12 +0530

    Change the Local API to Live API

--- src/components/API/Api.js
+++ src/components/API/Api.js
@@ -1,6 +1,6 @@
 // For Production API
-// export const ApiUrl = "https://cms.sultanpetdiocese.org/api";
+export const ApiUrl = "https://cms.kurnooldiocese.org/api";
 
 
 // For Development API
-export const ApiUrl = "http://127.0.0.1:8000/api";
+// export const ApiUrl = "http://127.0.0.1:8000/api";
